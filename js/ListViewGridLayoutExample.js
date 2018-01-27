c01eddfe6d217c683eb1182cbdb0e1ce6abb5af0
Douglas Lowder <dlowder@salesforce.com>
2018-01-26 22:55:01 -0800

    Fix imports in ListViewGridLayoutExample

--- js/ListViewGridLayoutExample.js
+++ js/ListViewGridLayoutExample.js
@@ -8,12 +8,9 @@
  */
 'use strict';
 
-var React, {
-  Component
-}  = require('react');
+import React, { Component } from 'react';
 
-var ReactNative = require('react-native');
-var {
+import {
   Image,
   ListView,
   Platform,
@@ -21,7 +18,7 @@ var {
   StyleSheet,
   Text,
   View,
-} = ReactNative;
+} from 'react-native';
 
 var THUMB_NAMES = [
   'like',
@@ -39,21 +36,15 @@ var THUMB_NAMES = [
 ];
 
 class ListViewGridLayoutExample extends Component {
-
-  statics: {
-    title: '<ListView> - Grid Layout',
-    description: 'Flexbox grid layout.'
-  }
-
   constructor(props: Object) {
     super(props);
-    let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    let ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     this.state = {
       dataSource: ds.cloneWithRows(this._genRows({})),
     };
   }
 
-  _pressData: {}
+  _pressData: {};
 
   componentWillMount() {
     this._pressData = {};
@@ -64,7 +55,7 @@ class ListViewGridLayoutExample extends Component {
       // ListView wraps ScrollView and so takes on its properties.
       // With that in mind you can use the ScrollView's contentContainerStyle prop to style the items.
       <ListView
-        removeClippedSubviews={this.props.removeClippedSubviews} 
+        removeClippedSubviews={this.props.removeClippedSubviews}
         contentContainerStyle={styles.list}
         dataSource={this.state.dataSource}
         initialListSize={21}
@@ -79,20 +70,21 @@ class ListViewGridLayoutExample extends Component {
     var rowHash = Math.abs(hashCode(rowData));
     var imgSource = THUMB_NAMES[rowHash % THUMB_NAMES.length];
     return (
-      <TouchableHighlight onPress={() => this._pressRow(rowID)} underlayColor='transparent'>
+      <TouchableHighlight
+        onPress={() => this._pressRow(rowID)}
+        underlayColor="transparent"
+      >
         <View>
           <View style={styles.row}>
-            <Image style={styles.thumb} source={{uri: imgSource}} />
-            <Text style={styles.text}>
-              {rowData}
-            </Text>
+            <Image style={styles.thumb} source={{ uri: imgSource }} />
+            <Text style={styles.text}>{rowData}</Text>
           </View>
         </View>
       </TouchableHighlight>
     );
   }
 
-  _genRows(pressData: {[key: number]: boolean}): Array<string> {
+  _genRows(pressData: { [key: number]: boolean }): Array<string> {
     var dataBlob = [];
     for (var ii = 0; ii < 100; ii++) {
       var pressedText = pressData[ii] ? ' (X)' : '';
@@ -103,17 +95,19 @@ class ListViewGridLayoutExample extends Component {
 
   _pressRow(rowID: number) {
     this._pressData[rowID] = !this._pressData[rowID];
-    this.setState({dataSource: this.state.dataSource.cloneWithRows(
-      this._genRows(this._pressData)
-    )});
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(
+        this._genRows(this._pressData),
+      ),
+    });
   }
-};
+}
 
 /* eslint no-bitwise: 0 */
 var hashCode = function(str) {
   var hash = 15;
   for (var ii = str.length - 1; ii >= 0; ii--) {
-    hash = ((hash << 5) - hash) + str.charCodeAt(ii);
+    hash = (hash << 5) - hash + str.charCodeAt(ii);
   }
   return hash;
 };
@@ -125,28 +119,28 @@ var styles = StyleSheet.create({
     justifyContent: 'space-around',
     flexDirection: 'row',
     flexWrap: 'wrap',
-    alignItems: 'flex-start'
+    alignItems: 'flex-start',
   },
   row: {
     justifyContent: 'center',
-    padding: scale*5,
-    margin: scale*3,
-    width: scale*100,
-    height: scale*100,
+    padding: scale * 5,
+    margin: scale * 3,
+    width: scale * 100,
+    height: scale * 100,
     backgroundColor: '#F6F6F6',
     alignItems: 'center',
-    borderWidth: scale*1,
-    borderRadius: scale*5,
-    borderColor: '#CCC'
+    borderWidth: scale * 1,
+    borderRadius: scale * 5,
+    borderColor: '#CCC',
   },
   thumb: {
-    width: scale*64,
-    height: scale*64
+    width: scale * 64,
+    height: scale * 64,
   },
   text: {
     flex: 1,
-    marginTop: scale*5,
-    fontWeight: 'bold'
+    marginTop: scale * 5,
+    fontWeight: 'bold',
   },
 });
 
