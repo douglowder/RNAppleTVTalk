6f192e0cd9784cd1e4d3cbfbf12cd46dee978eac
Douglas Lowder <dlowder@salesforce.com>
2019-06-23 22:12:42 -0700

    Move to cocoapods build

--- ios/RNAppleTVTalk/main.m
+++ ios/RNAppleTVTalk/main.m
@@ -1,18 +1,16 @@
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
+//
+//  main.m
+//  RNAppleTVTalk
+//
+//  Created by Douglas Lowder on 6/23/19.
+//  Copyright © 2019 Douglas Lowder. All rights reserved.
+//
 
 #import <UIKit/UIKit.h>
-
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
-  @autoreleasepool {
-    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-  }
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
 }
